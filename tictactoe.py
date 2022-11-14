43385d2a764298d3fdb829e60c465712b98f9f3d
Jonesgamings <harry.tennyson.jones@gmail.com>
2022-11-14 17:38:22 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,12 @@
+import pygame
+
 class Tile:
     
-    def __init__(self, pos) -> None:
-        self.pos = pos
+    def __init__(self, rect: pygame.Rect) -> None:
+        self.rect = rect
+        self.pos = (rect.x, rect.y)
+        self.width = rect.width
+        self.height = rect.height
         self.team = None
         self.clicked = False
 
@@ -13,5 +18,28 @@ class Tile:
 
         return False
 
-class Board:
-    pass
+class BoardWindow:
+
+    AI = "AI"
+    PLAYER = "PLAYER"
+    
+    def __init__(self, screen, size) -> None:
+        self.screen = screen
+        self.visible = False
+        self.size = size
+
+class MainMenu:
+
+    def __init__(self, screen) -> None:
+        self.screen = screen
+        self.visible = False
+
+class Screen:
+
+    def __init__(self, width, height) -> None:
+        self.boardSize = 3
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode([width, height])
+        self.boardWindow = BoardWindow(self, self.boardSize)
+        self.mainmanu = MainMenu(self)
