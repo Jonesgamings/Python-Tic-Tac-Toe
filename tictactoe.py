b447d44a2493f857afa1388242a38d34ddd07640
Jonesgamings <harry.tennyson.jones@gmail.com>
2022-11-14 21:52:55 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,8 @@
 import pygame
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 class Tile:
     
     def __init__(self, rect: pygame.Rect) -> None:
@@ -10,13 +13,18 @@ class Tile:
         self.team = None
         self.clicked = False
 
-    def setClick(self, team):
-        if not self.clicked:
-            self.clicked = True
-            self.team = team
-            return True
+    def checkClick(self, event, team):
+        if self.rect.collidepoint(event.pos):
+            if not self.clicked:
+                self.clicked = True
+                self.team = team
 
-        return False
+    def draw(self, screen):
+        if not self.clicked:
+            pygame.draw.rect(screen, BLACK, self.rect, 3)
+
+        else:
+            pygame.draw.rect(screen, BLACK, self.rect)
 
 class BoardWindow:
 
@@ -25,12 +33,16 @@ class BoardWindow:
     
     def __init__(self, screen, size, offset) -> None:
         self.screen: Screen = screen
-        self.visible = False
+        self.visible = True
         self.size = size
         self.tiles = {}
         self.gridX = 0
         self.gridY = 0
         self.offset = offset
+        self.gameType = None
+        self.currentTeam = None
+        
+        self.generateBlank()
 
     def generateBlank(self):
 
@@ -40,21 +52,41 @@ class BoardWindow:
         for y in range(self.size):
             for x in range(self.size):
                 pos = (y, x)
-                rect = pygame.rect(self.gridX * x, self.gridY * y, self.gridX, self.gridY)
+                realX = self.gridX * x + self.offset
+                realY = self.gridY * y + self.offset
+                rect = pygame.Rect(realX, realY, self.gridX, self.gridY)
                 tile = Tile(rect)
                 self.tiles[pos] = tile
 
+    def startGame(self, type_):
+        self.visible = True
+        self.gameType = type_
+        self.currentTeam = 1
+
+    def endGame(self):
+        self.visible = False
+        self.gameType = None
+        self.currentTeam = None
+
     def draw(self):
+        for tile in self.tiles.values():
+            tile.draw(self.screen.screen)
+
+    def update(self):
         pass
 
     def event(self, event):
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for tile in self.tiles.values():
+                tile.checkClick(event, self.currentTeam)
 
 class MainMenu:
 
     def __init__(self, screen) -> None:
         self.screen = screen
         self.visible = False
+        #self.AIButton = pygame.Rect()
+        #self.PlayerButton = pygame.Rect()
 
     def draw(self):
         pass
@@ -71,9 +103,12 @@ class Screen:
         self.screen = pygame.display.set_mode([width, height])
         self.boardWindow = BoardWindow(self, self.boardSize, 30)
         self.mainmanu = MainMenu(self)
+        self.running = False
 
     def update(self):
+        self.screen.fill(WHITE)
         if self.boardWindow.visible:
+            self.boardWindow.update()
             self.boardWindow.draw()
 
         if self.mainmanu.visible:
@@ -85,3 +120,35 @@ class Screen:
 
         if self.mainmanu.visible:
             self.mainmanu.event(event)
+
+    def loadGame(self, type_):
+        self.unloadMenu()
+        self.boardWindow.startGame(type_)
+
+    def unloadGame(self):
+        self.boardWindow.endGame()
+
+    def loadMenu(self):
+        self.mainmanu.visible = True
+
+    def unloadMenu(self):
+        self.mainmanu.visible = False
+
+    def mainloop(self):
+        self.running = True
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+                self.sendEvent(event)
+
+            self.update()
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    screen = Screen(1000, 1000)
+    screen.mainloop()
